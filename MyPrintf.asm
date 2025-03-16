69bdc2aaa369a91cbef905b995ae8d6773021e14
Michael Movsesian <michaelm0vsesian@yandex.ru>
2025-03-17 01:55:13 +0300

    %b added

--- MyPrintf.asm
+++ MyPrintf.asm
@@ -1,16 +1,17 @@
 global _start           ;Makes it visible for a linker from where to start
  
 section .data           ;Initialising variables in data segment
-String db "Hello %xworld!", 10, "$"   ;conditionally format string to print
+String db "Hello %bworld!", 10, "$"   ;conditionally format string to print
 StringLen equ $-String; length of the String to make things a little bit easier
 Count dq 0
+BufferLen dq 0
 section .bss
 Buffer resb 128
 
 section .text
 _start:
 
-    push 1760             ;unpaired push for giving arguments in function
+    push 113            ;unpaired push for giving arguments in function
     push String         ;
     call MyPrintf
 
@@ -35,14 +36,16 @@ MyPrintf:
     ;rsi - Format String pointer
     ;rdi - Buffer pointer
     
-    CreateBuffer:
+CreateBuffer:
 
     cmp byte [rsi], '%' 
     je PercentHandle
 
     movsb
 
-    Ready:
+    add qword [BufferLen], 1
+
+Ready:
     ;dec rcx
     ;cmp rcx, 0
     cmp byte [rsi], '$'
@@ -52,7 +55,7 @@ MyPrintf:
     sub rsp, qword [Count]          ;return pointer rsp where it was
 
     mov rsi, Buffer
-    mov rdx, StringLen  ; is to be replaced by function counting number of chars
+    mov rdx, BufferLen  ; is to be replaced by function counting number of chars
     mov rdi, 1          ; sets output in console
     ;rsi is given
     mov rax, 1          ; в RAX - номер функции для вывода в поток 
@@ -74,12 +77,17 @@ PercentHandle:
     cmp byte [rsi+1], 'x'
     je PrintHex
 
+    cmp byte [rsi+1], 'b'
+    je PrintBinary
+
+    ;cmp byte [rsi+1], 'o'
+    ;je PrintOctal
+
     inc rsi
     jmp Ready
-    ;cmp [rsi+1], 'd'
 
 
-    PrintPercent:
+PrintPercent:
     sub rsp, 8
     sub qword [Count], 8          ; it does not requiere argument to print %, so don`t counts this
     mov byte [rdi], '%'
@@ -87,7 +95,7 @@ PercentHandle:
     add rsi, 2          ; adding 2 as we have to skip another %
     jmp Ready
 
-    PrintDecimal:
+PrintDecimal:
     mov rax, [rsp] 
     add al, 30h
     mov byte [rdi], al
@@ -95,32 +103,48 @@ PercentHandle:
     add rsi, 2
     jmp Ready
 
-    PrintChar:
+PrintChar:
     mov rax, [rsp] 
     mov byte [rdi], al
     inc rdi
     add rsi, 2
     jmp Ready
 
-    PrintHex:
+PrintBinary:
 
-    ;mov rax, [rsp]
-    ;cqo          ; расширяем регистр rdx знаковым битом из RAX 
-    ;mov rbx, 16   ; 64-разрядный регистр
-    ;div rbx 
+    mov eax, 10000000000000000000000000000000b
+    ;mov cl, 31
 
-    ;call int_to_acsii
-    ;mov byte [rdi], al
-    ;add rdi, 1;
+PBIN:
 
-    ;add rsi, 2
-    ;jmp Ready
+    mov rbx, [rsp]
+    and ebx, eax
+    cmp ebx, 0
 
+    je Zero
+    mov bl, '1'
+    jmp PrtBl
+
+    Zero:
+    mov bl, '0' 
+
+    PrtBl:
+    mov byte [rdi], bl
+    inc rdi
+
+    shr rax, 1
+
+    cmp eax, 0h
+    jne PBIN
+
+    add rsi, 2
+    jmp Ready
+
+PrintHex:
     mov eax, 0f0000000h
     mov cl, 28
-    ;mov rbp, 28
 
-    PHEX:
+PHEX:
 
     mov rbx, [rsp]
     and ebx, eax
@@ -131,40 +155,12 @@ PercentHandle:
 
     shr rax, 4
     sub cl, 4
-    ;mov Count, cl
 
     cmp eax, 0h
     jne PHEX
 
     add rsi, 2
     jmp Ready
-
-    ;mov rbx, rax
-    ;and ebx, 0ff000000h
-    ;shr ebx, 24 
-    ;call int_to_acsii
-    ;mov byte [rdi], bl
-    ;inc rdi
-
-    ;mov ebx, eax
-    ;and ebx, 00ff0000h
-    ;shr ebx, 16 
-    ;call int_to_acsii
-    ;mov byte [rdi], bl
-    ;inc rdi
-
-    ;mov ebx, eax
-    ;and ebx, 000000f0h
-    ;shr ebx, 8 
-    ;call int_to_acsii
-    ;mov byte [rdi], bl
-    ;inc rdi
-
-    ;mov ebx, eax
-    ;and ebx, 0000000fh 
-    ;call int_to_acsii
-    ;mov byte [rdi], bl
-    ;inc rdi    
     
 int_to_acsii:
     cmp bl, 10
